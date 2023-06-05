0ec2e596d10a41dc5d4a0c05e9d308793375179b
Marine Desmier <desmier.marine@bbox.fr>
2023-06-05 12:43:28 +0200

    commit

--- src/main/java/fr/iocean/species/controller/SpeciesController.java
+++ src/main/java/fr/iocean/species/controller/SpeciesController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.iocean.species.model.Species;
 import fr.iocean.species.repository.SpeciesRepository;
@@ -16,23 +17,23 @@ import fr.iocean.species.repository.SpeciesRepository;
 public class SpeciesController {
 	@Autowired
 	private SpeciesRepository speciesRepository;
-	
+
 	@GetMapping("species")
 	public String getListSpecies(Model model) {
 		// 1er etapes recup les spacies via le reop
 		List<Species> speciesList = speciesRepository.findAll();
-		
-		//2eme mettre la list dans le model
+
+		// 2eme mettre la list dans le model
 		model.addAttribute("speciesList", speciesList);
-		
+
 		// 3eme retourne la vue
 		return "species/list_species";
 	}
-	
+
 	@GetMapping("species/{id}")
 	public String getDetailSpecies(@PathVariable("id") Integer id, Model model) {
 		Optional<Species> speciesOpt = speciesRepository.findById(id);
-		if(speciesOpt.isEmpty()) {
+		if (speciesOpt.isEmpty()) {
 			// pas de species avec l'id renseigné
 			return "species/error";
 		}
@@ -40,11 +41,18 @@ public class SpeciesController {
 		// 3eme retourne la vue
 		return "species/detail_species";
 	}
-	
+
 	@GetMapping("species/create")
 	public String getCreateSpecies(Model model) {
 		model.addAttribute("newSpecies", new Species());
 		// 3eme retourne la vue
 		return "species/create_species";
 	}
+
+	@PostMapping("/species")
+	public String createOrUpdate(Species speciesItem) {
+		this.speciesRepository.save(speciesItem);
+		return "redirect:/species";
+	}
+
 }
